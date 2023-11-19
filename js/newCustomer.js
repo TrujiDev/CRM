d737da9336a1932f8692b89658096cd94f8927d3
TrujiDev <eetgrisales@gmail.com>
2023-11-19 10:50:03 -0500

    Add customer creation functionality

--- js/newCustomer.js
+++ js/newCustomer.js
@@ -21,6 +21,10 @@
 		};
 	}
 
+/**
+ * Validates the customer information and creates a new customer.
+ * @param {Event} event - The event object.
+ */
 	function validateCustomer(event) {
 		event.preventDefault();
 
@@ -33,14 +37,49 @@
 			printAlert('All fields are required', 'error');
 			return;
 		}
+
+		// Create a object with the information
+		const customer = {
+			name,
+			email,
+			phone,
+			company,
+			id: Date.now(),
+		};
+
+		createNewCustomer(customer);
 	}
 
 /**
- * Prints an alert message on the screen.
- * 
- * @param {string} message - The message to be displayed in the alert.
- * @param {string} type - The type of alert. Possible values are 'error' or any other value for success alert.
+ * Creates a new customer and adds it to the CRM database.
+ * @param {Object} customer - The customer object to be added.
  */
+	function createNewCustomer(customer) {
+		const transaction = DB.transaction(['crm'], 'readwrite');
+
+		const objectStore = transaction.objectStore('crm');
+
+		objectStore.add(customer);
+
+		transaction.onerror = function () {
+			printAlert('There was an error', 'error');
+		};
+
+		transaction.oncomplete = function () {
+			printAlert('The customer was added', 'success');
+
+			setTimeout(() => {
+				window.location.href = 'index.html';
+			}, 3000);
+		};
+	}
+
+	/**
+	 * Prints an alert message on the screen.
+	 *
+	 * @param {string} message - The message to be displayed in the alert.
+	 * @param {string} type - The type of alert. Possible values are 'error' or any other value for success alert.
+	 */
 	function printAlert(message, type) {
 		const alert = document.querySelector('.alert');
 
