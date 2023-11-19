df2eeb254ad1c9d020b0543269390496ae374a51
TrujiDev <eetgrisales@gmail.com>
2023-11-19 12:16:16 -0500

    Refactor newCustomer.js file

--- js/newCustomer.js
+++ js/newCustomer.js
@@ -9,22 +9,10 @@
 		form.addEventListener('submit', validateCustomer);
 	});
 
-	function connectDB() {
-		const openConnection = window.indexedDB.open('crm', 1);
-
-		openConnection.onerror = function () {
-			console.log('There was an error');
-		};
-
-		openConnection.onsuccess = function () {
-			DB = openConnection.result;
-		};
-	}
-
-/**
- * Validates the customer information and creates a new customer.
- * @param {Event} event - The event object.
- */
+	/**
+	 * Validates the customer information and creates a new customer.
+	 * @param {Event} event - The event object.
+	 */
 	function validateCustomer(event) {
 		event.preventDefault();
 
@@ -50,10 +38,10 @@
 		createNewCustomer(customer);
 	}
 
-/**
- * Creates a new customer and adds it to the CRM database.
- * @param {Object} customer - The customer object to be added.
- */
+	/**
+	 * Creates a new customer and adds it to the CRM database.
+	 * @param {Object} customer - The customer object to be added.
+	 */
 	function createNewCustomer(customer) {
 		const transaction = DB.transaction(['crm'], 'readwrite');
 
@@ -72,47 +60,5 @@
 				window.location.href = 'index.html';
 			}, 3000);
 		};
-	}
-
-	/**
-	 * Prints an alert message on the screen.
-	 *
-	 * @param {string} message - The message to be displayed in the alert.
-	 * @param {string} type - The type of alert. Possible values are 'error' or any other value for success alert.
-	 */
-	function printAlert(message, type) {
-		const alert = document.querySelector('.alert');
-
-		if (!alert) {
-			const divMessage = document.createElement('div');
-			divMessage.classList.add(
-				'px-4',
-				'py-3',
-				'rounded',
-				'max-w-lg',
-				'mx-auto',
-				'mt-6',
-				'text-center',
-				'border',
-				'alert'
-			);
-
-			if (type === 'error') {
-				divMessage.classList.add('bg-red-100', 'border-red-400', 'text-red-700');
-			} else {
-				divMessage.classList.add(
-					'bg-green-100',
-					'border-green-400',
-					'text-green-700'
-				);
-			}
-
-			divMessage.textContent = message;
-			form.appendChild(divMessage);
-
-			setTimeout(() => {
-				divMessage.remove();
-			}, 3000);
-		}
 	}
 })();
